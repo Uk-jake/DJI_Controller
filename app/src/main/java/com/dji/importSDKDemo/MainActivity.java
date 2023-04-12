1c12db04b0274e61ed97461205da2e921a7117f8
Uk-jake <mag0225@naver.com>
2023-04-12 14:03:31 -0400

    Add_MainActivity

--- app/src/main/java/com/dji/importSDKDemo/MainActivity.java
+++ app/src/main/java/com/dji/importSDKDemo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.dji.importSDKDemo;
 
 import android.Manifest;
+import android.animation.AnimatorInflater;
+import android.animation.LayoutTransition;
+import android.animation.ObjectAnimator;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.usb.UsbManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,18 +15,28 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.dji.importSDKDemo.utils.ToastUtils;
+import com.dji.importSDKDemo.view.PresentableView;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import dji.common.error.DJIError;
@@ -33,9 +49,22 @@ import dji.sdk.sdkmanager.DJISDKManager;
 public class MainActivity extends AppCompatActivity{
 
     private static final String TAG = MainActivity.class.getName();
+
+    private FrameLayout contentFrameLayout;
+
     public static final String FLAG_CONNECTION_CHANGE = "dji_sdk_connection_change";
     private static BaseProduct mProduct;
-    private Handler mHandler;
+    private SearchView searchView;
+    private MenuItem searchViewItem;
+    private MenuItem hintItem;
+    private ObjectAnimator pushInAnimator;
+    private ObjectAnimator pushOutAnimator;
+    private TextView titleTextView;
+
+    private ObjectAnimator popInAnimator;
+    private LayoutTransition popOutTransition;
+
+    private Stack<ViewWrapper> stack;
 
     private static final String[] REQUIRED_PERMISSION_LIST = new String[]{
             //권한 설정하기 위해
@@ -57,206 +86,213 @@ public class MainActivity extends AppCompatActivity{
     private AtomicBoolean isRegistrationInProgress = new AtomicBoolean(false);
     private static final int REQUEST_PERMISSION_CODE = 12345;
 
+    //region Life-cycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // When the compile and target version is higher than 22, please request the following permission at runtime to ensure the SDK works well.
-        // 1차로 SDK version 확인
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            //SDK를 사용하기 위해 필요한 권한 확인
-            checkAndRequestPermissions();
-
-
-        }
-
-        //activity_main layout 띄움.
+        DJISampleApplication.getEventBus().register(this);
         setContentView(R.layout.activity_main);
-
-        //Initialize DJI SDK Manager
-        mHandler = new Handler(Looper.getMainLooper());
-
-    }
-
-    /**
-     * Checks if there is any missing permissions, and
-     * requests runtime permission if needed.
-     */
-    private void checkAndRequestPermissions() {
-        // Check for permissions
-        for (String eachPermission : REQUIRED_PERMISSION_LIST) {
-            if (ContextCompat.checkSelfPermission(this, eachPermission) != PackageManager.PERMISSION_GRANTED) {
-                missingPermission.add(eachPermission);
-            }
-        }
-
-        // Request for missing permissions
-        if (missingPermission.isEmpty()) {
-            startSDKRegistration();
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            showToast("Need to grant the permissions!");
-            ActivityCompat.requestPermissions(this,
-                    missingPermission.toArray(new String[missingPermission.size()]),
-                    REQUEST_PERMISSION_CODE);
-        }
-
-    }
-
-    /**
-     * Result of runtime permission request
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // Check for granted permission and remove from missing list
-        if (requestCode == REQUEST_PERMISSION_CODE) {
-            for (int i = grantResults.length - 1; i >= 0; i--) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                    missingPermission.remove(permissions[i]);
-                }
-            }
-        }
-        // If there is enough permission, we will start the registration
-        if (missingPermission.isEmpty()) {
-            startSDKRegistration();
-        } else {
-            showToast("Missing permissions!!!");
-        }
-    }
-
-    private void startSDKRegistration() {
-        if (isRegistrationInProgress.compareAndSet(false, true)) {
-            AsyncTask.execute(new Runnable() {
-                @Override
-                public void run() {
-                    showToast("registering, pls wait...");
-
-                    DJISDKManager.getInstance().registerApp(MainActivity.this.getApplicationContext(), new DJISDKManager.SDKManagerCallback() {
-                        @Override
-                        public void onRegister(DJIError djiError) {
-                            if (djiError == DJISDKError.REGISTRATION_SUCCESS) {
-                                showToast("Register Success");
-                                DJISDKManager.getInstance().startConnectionToProduct();
-                            } else {
-                                showToast("Register sdk fails, please check the bundle id and network connection!");
-                            }
-                            Log.v(TAG, djiError.getDescription());
-                        }
-
-                        @Override
-                        public void onProductDisconnect() {
-                            Log.d(TAG, "onProductDisconnect");
-                            showToast("Product Disconnected");
-                            notifyStatusChange();
-
-                        }
-                        @Override
-                        public void onProductConnect(BaseProduct baseProduct) {
-                            Log.d(TAG, String.format("onProductConnect newProduct:%s", baseProduct));
-                            showToast("Product Connected");
-                            notifyStatusChange();
-
-                        }
-
-                        @Override
-                        public void onProductChanged(BaseProduct baseProduct) {
-
-                        }
-
-                        @Override
-                        public void onComponentChange(BaseProduct.ComponentKey componentKey, BaseComponent oldComponent,
-                                                      BaseComponent newComponent) {
-
-                            if (newComponent != null) {
-                                newComponent.setComponentListener(new BaseComponent.ComponentListener() {
-
-                                    @Override
-                                    public void onConnectivityChange(boolean isConnected) {
-                                        Log.d(TAG, "onComponentConnectivityChanged: " + isConnected);
-                                        notifyStatusChange();
-                                    }
-                                });
-                            }
-                            Log.d(TAG,
-                                    String.format("onComponentChange key:%s, oldComponent:%s, newComponent:%s",
-                                            componentKey,
-                                            oldComponent,
-                                            newComponent));
-
-                        }
-
-                        @Override
-                        public void onInitProcess(DJISDKInitEvent djisdkInitEvent, int i) {
-
-                        }
-
-                        @Override
-                        public void onDatabaseDownloadProgress(long l, long l1) {
-
-                        }
-
-                    });
-                }
-            });
-        }
-    }
-
-
-    private void notifyStatusChange() {
-        mHandler.removeCallbacks(updateRunnable);
-        mHandler.postDelayed(updateRunnable, 500);
-    }
-
-    private Runnable updateRunnable = new Runnable() {
-
-        @Override
-        public void run() {
-            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
-            sendBroadcast(intent);
-        }
-    };
-
-    private void showToast(final String toastMsg) {
-
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(getApplicationContext(), toastMsg, Toast.LENGTH_LONG).show();
-            }
-        });
-
-    }
-
-    @Override
-    public void onResume() {
-        Log.e(TAG, "onResume");
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        Log.e(TAG, "onPause");
-        super.onPause();
-    }
-
-    @Override
-    public void onStop() {
-        Log.e(TAG, "onStop");
-        super.onStop();
-    }
-
-    public void onReturn(View view){
-        Log.e(TAG, "onReturn");
-        this.finish();
+        setupActionBar();
+        contentFrameLayout = (FrameLayout) findViewById(R.id.framelayout_content);
+        initParams();
+//        // When the compile and target version is higher than 22, please request the following permission at runtime to ensure the SDK works well.
+//        // 1차로 SDK version 확인
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            //SDK를 사용하기 위해 필요한 권한 확인
+//            checkAndRequestPermissions();
+//        }
+//        //activity_main layout 띄움.
+//        setContentView(R.layout.activity_main);
+//        //Initialize DJI SDK Manager
+//        mHandler = new Handler(Looper.getMainLooper());
     }
 
     @Override
     protected void onDestroy() {
-        Log.e(TAG, "onDestroy");
+        DJISampleApplication.getEventBus().unregister(this);
         super.onDestroy();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchViewItem = menu.findItem(R.id.action_search);
+        hintItem = menu.findItem(R.id.action_hint);
+        searchView = (SearchView) searchViewItem.getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                DJISampleApplication.getEventBus().post(new SearchQueryEvent(""));
+                return false;
+            }
+        });
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                DJISampleApplication.getEventBus().post(new SearchQueryEvent(query));
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                DJISampleApplication.getEventBus().post(new SearchQueryEvent(newText));
+                return false;
+            }
+        });
+
+        // Hint click
+        hintItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                showHint();
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    protected void onNewIntent(@NonNull Intent intent) {
+        super.onNewIntent(intent);
+        String action = intent.getAction();
+        if (UsbManager.ACTION_USB_ACCESSORY_ATTACHED.equals(action)) {
+            Intent attachedIntent = new Intent();
+            attachedIntent.setAction(DJISDKManager.USB_ACCESSORY_ATTACHED);
+            sendBroadcast(attachedIntent);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (stack.size() > 1) {
+            popView();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (null != actionBar) {
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            actionBar.setCustomView(R.layout.actionbar_custom);
+
+            titleTextView = (TextView) (actionBar.getCustomView().findViewById(R.id.title_tv));
+        }
+    }
+
+    public static class SearchQueryEvent {
+        private final String query;
+
+        public SearchQueryEvent(String query) {
+            this.query = query;
+        }
+
+        public String getQuery() {
+            return query;
+        }
+
+    }
+
+    private void setupInAnimations() {
+        pushInAnimator = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.slide_in_right);
+        pushOutAnimator = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.fade_out);
+        popInAnimator = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.fade_in);
+        ObjectAnimator popOutAnimator =
+                (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.slide_out_right);
+
+        pushOutAnimator.setStartDelay(100);
+
+        popOutTransition = new LayoutTransition();
+        popOutTransition.setAnimator(LayoutTransition.DISAPPEARING, popOutAnimator);
+        popOutTransition.setDuration(popOutAnimator.getDuration());
+    }
+
+
+    private void initParams() {
+        setupInAnimations();
+
+        stack = new Stack<ViewWrapper>();
+        View view = contentFrameLayout.getChildAt(0);
+        stack.push(new ViewWrapper(view, R.string.activity_component_list));
+    }
+
+
+    private void showHint() {
+        if (stack.size() != 0 && stack.peek().getView() instanceof PresentableView) {
+            ToastUtils.setResultToToast(((PresentableView) stack.peek().getView()).getHint());
+        }
+    }
+
+    private void popView() {
+
+        if (stack.size() <= 1) {
+            finish();
+            return;
+        }
+
+        ViewWrapper removeWrapper = stack.pop();
+
+        View showView = stack.peek().getView();
+        View removeView = removeWrapper.getView();
+
+        contentFrameLayout.setLayoutTransition(popOutTransition);
+        contentFrameLayout.removeView(removeView);
+
+        popInAnimator.setTarget(showView);
+        popInAnimator.start();
+
+        refreshTitle();
+        //refreshOptionsMenu();
+    }
+
+    private void refreshTitle() {
+        if (stack.size() > 1) {
+            ViewWrapper wrapper = stack.peek();
+            titleTextView.setText(wrapper.getTitleId());
+        } else if (stack.size() == 1) {
+            BaseProduct product = DJISampleApplication.getProductInstance();
+            if (product != null && product.getModel() != null) {
+                titleTextView.setText("" + product.getModel().getDisplayName());
+            } else {
+                titleTextView.setText(R.string.sample_app_name);
+            }
+        }
+    }
+
+    private void refreshOptionsMenu() {
+//        if (stack.size() == 2 && stack.peek().getView() instanceof DemoListView) {
+//            searchViewItem.setVisible(true);
+//        } else {
+//            searchViewItem.setVisible(false);
+//            searchViewItem.collapseActionView();
+//        }
+//        if (stack.size() == 3 && stack.peek().getView() instanceof PresentableView) {
+//            hintItem.setVisible(true);
+//        } else {
+//            hintItem.setVisible(false);
+//        }
+    }
+
+    public static class RequestStartFullScreenEvent {
+    }
+
+    public static class RequestEndFullScreenEvent {
+    }
+
+    public static class ConnectivityChangeEvent {
+    }
+
+
+
+
 
 }
